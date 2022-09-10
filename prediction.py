25471e929a97dd00671df56a2cd4fa2ab0a9e4f4
ZICCORP <frankchuka250@gmail.com>
2022-09-10 15:13:41 +0100

    complete project

---
+++ prediction.py
@@ -0,0 +1,12 @@
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn.model_selection import train_test_split
+# from sklearn.svm import  SVC
+# from sklearn.metrics import f1_score,accuracy_score,confusion_matrix
+# import seaborn as sns
+# import os
+
+# print('Frank Chuka')
+
+# import keras
